0496e7c55f2c6a73287a8ccf2be1a4629679d0ff
Imagic <imagic@imagic.com>
2017-05-05 00:05:23 +0200

    Add item config file

---
+++ vrp/cfg/items.lua
@@ -0,0 +1,10 @@
+-- define items, see the Inventory API on github
+
+local cfg = {}
+
+-- idname = {name,description,choices}
+cfg.items = {
+  ["weed"] = {"Weed", "Some weed.", {}} -- no choices
+}
+
+return cfg
--- vrp/modules/basic_items.lua
+++ vrp/modules/basic_items.lua
@@ -34,3 +34,10 @@ sd_choices["Eat"] = {function(player,choice)
 end}
 
 vRP.defInventoryItem("sandwich","Sandwich","A tasty snack.",sd_choices)
+
+-- load config items
+local cfg = require("resources/vrp/cfg/items")
+
+for k,v in pairs(cfg.items) do
+  vRP.defInventoryItem(k,v[1],v[2],v[3])
+end
