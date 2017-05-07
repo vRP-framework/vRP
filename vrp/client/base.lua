444789a29bdd382c7420af09aae7b08de19b64df
3rg0 <3rg00@users.noreply.github.com>
2017-05-07 10:39:17 +0200

    add lock vehicles function

--- vrp/client/base.lua
+++ vrp/client/base.lua
@@ -139,6 +139,53 @@ function tvRP.playFullAnim(dict,name)
   end)
 end
 
+-- vehicles lock/unlock
+function tvRP.lockVehicle()
+    local distanceParam = 5 -- Change this value to change the distance needed to lock / unlock a vehicle
+    local key = 303 -- Change this value to change the key (List of values below)
+    local nveh, posnveh = 0, 0
+    local player = GetPlayerPed(-1)
+    local nveh = SetPlayersLastVehicle(GetVehiclePedIsIn(player, true)) 
+    local pReg = tvRP.getRegistrationNumber(player)
+    local pLock = GetVehicleNumberPlateText(nveh) -- need to substring
+    if nveh ~= 0 then 
+        posnveh = GetEntityCoords(nveh, false)
+        nvehX, nvehY, nvehZ = posnveh.x, posnveh.y, posnveh.z 
+
+        posPlayer = GetEntityCoords(player, false) 
+        playerX, playerY, playerZ = posPlayer.x, posPlayer.y, posPlayer.z 
+    end
+    if IsControlJustPressed(1, key) then
+        if nveh == 0 then
+            tvRP.notify("You don't have a vehicle.")
+        elseif nveh ~= 0 and pReg == pLock then   
+            distanceBetweenVehPlayer = GetDistanceBetweenCoords(nvehX, nvehY, nvehZ, playerX, playerY, playerZ, false)
+            --IsPedGettingIntoAVehicle(player)
+            --Citizen.Trace("Vous essayez d'entrer dans le véhicule") 
+            if distanceBetweenVehPlayer <= distanceParam then 
+                lockStatus = GetVehicleDoorLockStatus(nveh) 
+                if lockStatus == 1 or lockStatus == 0 then 
+                    engineValue = IsPedInAnyVehicle(player)
+                    lockStatus = SetVehicleDoorsLocked(nveh, 2)
+                    SetVehicleDoorsLockedForPlayer(nveh, PlayerId(), false)
+                    tvRP.notify("Vehicle Locked.")    
+                    if not engineValue then
+                        SetVehicleEngineOn(nveh, false, false, true)
+                    end
+
+                else 
+                    lockStatus = SetVehicleDoorsLocked(nveh, 1)
+                    tvRP.notify("Vehicle Unlocked.")
+                end
+            else 
+            tvRP.notify("You are too far from the vehicle.")
+            end
+        else
+        tvRP.notify("This is not your vehicle...")
+        end  
+    end
+end
+
 -- events
 
 AddEventHandler("playerSpawned",function()
