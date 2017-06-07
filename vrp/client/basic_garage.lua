589818d7c95116f046b8af9cd9066a1579da1177
Imagic <imagic@imagic.com>
2017-06-07 16:48:40 +0200

    Fix put in vehicle for police cars, helicos and boats

--- vrp/client/basic_garage.lua
+++ vrp/client/basic_garage.lua
@@ -66,7 +66,14 @@ function tvRP.getNearestVehicle(radius)
   if IsPedSittingInAnyVehicle(ped) then
     return GetVehiclePedIsIn(ped, true)
   else
-    return GetClosestVehicle(x+0.0001,y+0.0001,z+0.0001, radius+0.0001, 0, 7) 
+    -- flags used:
+    --- 8192: boat
+    --- 4096: helicos
+    --- 4,2,1: cars (with police)
+
+    local veh = GetClosestVehicle(x+0.0001,y+0.0001,z+0.0001, radius+0.0001, 0, 8192+4096+4+2+1)  -- boats, helicos
+    if not IsEntityAVehicle(veh) then veh = GetClosestVehicle(x+0.0001,y+0.0001,z+0.0001, radius+0.0001, 0, 4+2+1) end -- cars
+    return veh
   end
 end
 
