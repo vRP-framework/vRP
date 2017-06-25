31a8f5dc8287d5643b79b3eff0c26bf142c52409
Imagic <imagic@imagic.com>
2017-06-25 16:01:46 +0200

    Add body training example.

--- vrp/cfg/blips_markers.lua
+++ vrp/cfg/blips_markers.lua
@@ -1,11 +1,13 @@
 
 -- this file is used to define additional static blips and markers to the map
+-- some lists: https://wiki.gtanet.work/index.php?title=Blips
 
 local cfg = {}
 
 -- list of blips
 -- {x,y,z,idtype,idcolor,text}
 cfg.blips = {
+  {-1202.96252441406,-1566.14086914063,4.61040639877319,311,17,"Body training"}
 }
 
 -- list of markers
--- vrp/cfg/item_transformers.lua
+++ vrp/cfg/item_transformers.lua
@@ -22,9 +22,6 @@ cfg.item_transformers = {
         reagents={}, -- items taken per unit
         products={ -- items given per unit
           ["water"] = 1
-        },
-        aptitudes={ -- optional
-          ["physical.strength"] = 1 -- "group.aptitude", give 1 exp per unit
         }
       },
       ["Harvest tacos"] = { -- action name
@@ -40,6 +37,26 @@ cfg.item_transformers = {
     --, onstart = function(player,recipe) end, -- optional start callback
     -- onstep = function(player,recipe) end, -- optional step callback
     -- onstop = function(player,recipe) end -- optional stop callback
+  },
+  {
+    name="Body training", -- menu name
+    r=255,g=125,b=0, -- color
+    max_units=1000,
+    units_per_minute=1000,
+    x=-1202.96252441406,y=-1566.14086914063,z=4.61040639877319,
+    radius=7.5, height=1.5, -- area
+    recipes = {
+      ["Strength"] = { -- action name
+        description="Increase your strength.", -- action description
+        in_money=0, -- money taken per unit
+        out_money=0, -- money earned per unit
+        reagents={}, -- items taken per unit
+        products={}, -- items given per unit
+        aptitudes={ -- optional
+          ["physical.strength"] = 1 -- "group.aptitude", give 1 exp per unit
+        }
+      }
+    }
   }
 }
 
