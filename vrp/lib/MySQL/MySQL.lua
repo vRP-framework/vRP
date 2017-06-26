b3d588329fffa09f63d8f260c69cfd6b9b2c30bb
Raid <r4ideddesigns@gmail.com>
2017-06-26 17:53:00 +1000

    Did not commit removal of original lines.
    Awkward...

--- vrp/lib/MySQL/MySQL.lua
+++ vrp/lib/MySQL/MySQL.lua
@@ -208,7 +208,6 @@ end
 local MySQL = {}
 
 -- host can be "host" or "host:port"
-function MySQL.open(host,user,password,db,debug)
 function MySQL.open(host,port,user,password,db,debug)
   local r = setmetatable({},{ __index = Connection })
 
@@ -218,7 +217,6 @@ function MySQL.open(host,port,user,password,db,debug)
     host = host_parts[1]..";port="..host_parts[2]
   end
 
-  r.connection = lib.MySqlClient.MySqlConnection("server="..host..";uid="..user..";pwd="..password..";database="..db..";")
   r.connection = lib.MySqlClient.MySqlConnection("server="..host..";port="..port..";uid="..user..";pwd="..password..";database="..db..";")
   r.connection.Open()
   r.debug = debug
