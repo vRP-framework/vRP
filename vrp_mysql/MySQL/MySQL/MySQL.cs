ed3a98bdc2574feba0eb06721af1045eef1a8065
Imagic <imagic@imagic.com>
2017-07-15 15:16:33 +0200

    Test change.

--- vrp_mysql/MySQL/MySQL/MySQL.cs
+++ vrp_mysql/MySQL/MySQL/MySQL.cs
@@ -144,6 +144,7 @@ namespace vRP
             else{
               dict["status"] = -1;
               tasks.Remove((uint)id);
+              Console.WriteLine("[vRP/C#] task null result");
               return dict;
             }
           }
@@ -155,10 +156,12 @@ namespace vRP
         else{
           tasks.Remove((uint)id);
           dict["status"] = -1;
+          Console.WriteLine("[vRP/C#] task faulted");
           return dict;
         }
       }
       else{
+        Console.WriteLine("[vRP/C#] task missing");
         dict["status"] = -1;
         return dict;
       }
