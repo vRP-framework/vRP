6e2df246bc833f399157f297e1427dbba58d1770
Imagic <imagic@imagic.com>
2017-07-15 14:01:12 +0200

    Test change.

--- vrp_mysql/MySQL/MySQL/MySQL.cs
+++ vrp_mysql/MySQL/MySQL/MySQL.cs
@@ -34,7 +34,7 @@ namespace vRP
       Exports.Add("createConnection", new Action<string,string>(e_createConnection));
       Exports.Add("createCommand", new Action<string,string>(e_createCommand));
       Exports.Add("query", new Func<string,IDictionary<string,object>,int>(e_query));
-      Exports.Add("checkTask", new Func<int,Dictionary<string,object>>(e_checkTask));
+      Exports.Add("checkTask", new Func<int,IDictionary<string,object>>(e_checkTask));
     }
 
     //return [con,cmd] from "con/cmd"
@@ -116,9 +116,9 @@ namespace vRP
       return task;
     }
 
-    public Dictionary<string,object> e_checkTask(int id)
+    public IDictionary<string,object> e_checkTask(int id)
     {
-      Dictionary<string, object> dict = new Dictionary<string,object>();
+      IDictionary<string, object> dict = new Dictionary<string,object>();
 
       Task<object[]> task = null;
       if(tasks.TryGetValue((uint)id, out task)){
