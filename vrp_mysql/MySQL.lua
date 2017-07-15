9fa512d0d0da550b3bdf4470361e52454005972c
Imagic <imagic@imagic.com>
2017-07-15 13:44:46 +0200

    Test changes.

--- vrp_mysql/MySQL.lua
+++ vrp_mysql/MySQL.lua
@@ -5,7 +5,7 @@ local tasks = {}
 local function tick()
   local rmtasks = {}
   for id,cb in pairs(tasks) do
-    local data = exports.vrp_mysql.checkTask(id)
+    local data = exports.vrp_mysql:checkTask(id)
     if data[1] then -- ok
       cb(data[2],data[3]) -- rows, affected
       table.insert(rmtasks, id)
--- vrp_mysql/MySQL/MySQL/MySQL.cs
+++ vrp_mysql/MySQL/MySQL/MySQL.cs
@@ -34,7 +34,7 @@ namespace vRP
       Exports.Add("createConnection", new Action<string,string>(e_createConnection));
       Exports.Add("createCommand", new Action<string,string>(e_createCommand));
       Exports.Add("query", new Func<string,IDictionary<string,object>,int>(e_query));
-      Exports.Add("checkTask", new Func<int,object>(e_checkTask));
+      Exports.Add("checkTask", new Func<uint,object>(e_checkTask));
     }
 
     //return [con,cmd] from "con/cmd"
@@ -116,19 +116,25 @@ namespace vRP
       return task;
     }
 
-    public object e_checkTask(int id)
+    public object e_checkTask(uint id)
     {
+      Dictionary<string, object> dict = new Dictionary<string,object>();
+
       Task<object[]> task = null;
-      if(tasks.TryGetValue((uint)id, out task)){
+      if(tasks.TryGetValue(id, out task)){
         if(!task.IsFaulted && task.IsCompleted){
           var r = (object[])task.Result;
           Console.WriteLine("[vRP/C#] send back mysql result to "+id);
-          tasks.Remove((uint)id);
-          return new object[]{true, r[0], r[1]};
+          tasks.Remove(id);
+          dict.Add("ok",true);
+          dict.Add("rows",r[0]);
+          dict.Add("affected",r[1]);
+          return dict;
         }
       }
 
-      return new object[]{false};
+      dict.Add("ok",false);
+      return dict;
     }
   }
 }
