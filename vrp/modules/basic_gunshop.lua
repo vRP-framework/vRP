87e83873d97cc3c8cbc58af6e1f8fe6d47c1c929
Guilherme Stoppa Caulada <guistoppa1995@gmail.com>
2017-12-01 00:35:15 -0200

    Fix #357

--- vrp/modules/basic_gunshop.lua
+++ vrp/modules/basic_gunshop.lua
@@ -30,6 +30,7 @@ for gtype,weapons in pairs(gunshop_types) do
         -- prompt amount
         vRP.prompt(player,lang.gunshop.prompt_ammo({choice}),"",function(player,amount)
           local amount = parseInt(amount)
+          if amount > 250 then amount = 250 end
           if amount >= 0 then
             local user_id = vRP.getUserId(player)
             local total = math.ceil(parseFloat(price_ammo)*parseFloat(amount))
