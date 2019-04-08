6845df0b4671c284c2c4f464120e895404e19ca7
André <tabarra@vexguard.net>
2019-04-08 01:50:30 -0300

    Fixed misaligned health bar
    Before: https://i.imgur.com/LcyIsjZ.jpg  
    After: https://i.imgur.com/utVGCPQ.png

--- vrp/cfg/client.lua
+++ vrp/cfg/client.lua
@@ -16,8 +16,8 @@ cfg.audio_listener_rate = 15 -- audio listener position update rate
 cfg.audio_listener_on_player = false -- set the listener position on the player instead of the camera
 
 cfg.gui = {
-  anchor_minimap_width = 260,
-  anchor_minimap_left = 60,
+  anchor_minimap_width = 271,
+  anchor_minimap_left = 29,
   anchor_minimap_bottom = 213
 }
 
